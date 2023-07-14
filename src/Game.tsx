3daac79beddd02cf2486bcc2ff4f454b05e8556e
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-13 20:02:22 -0400

    initial commit

---
+++ src/Game.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Game() {
+
+  return (
+    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe</h1>
+      </div>
+  )
+}
+
+// export default Game;
