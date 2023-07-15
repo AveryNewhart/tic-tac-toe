60e3a5161eee8d2e98c591e0cddc656b182490cd
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-15 15:04:25 -0400

    onclick working for the x

--- src/Game.tsx
+++ src/Game.tsx
@@ -7,13 +7,27 @@ const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""]
 export default function Game() {
 
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE)
+  const [currentPlayer, setCurrentPlayer] = useState("X")
+
+  const handleCellClick = (event: any) => {
+    const cellIndex = Number(event.target.getAttribute("data-cell-index"))
+
+    const currentValue = gameState[cellIndex];
+    if (currentValue) {
+      return 
+    }
+
+    const newValues = [...gameState]
+    newValues[cellIndex] = currentPlayer
+    setGameState(newValues);
+  };
 
   return (
     <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe</h1>
       <div className='grid grid-cols-3 gap-3 mx-auto w-96'>
         {gameState.map((player, index) => (
-        <Square key={index} {...{ player }}/>
+        <Square key={index} onClick={handleCellClick} {...{ index, player }}/>
       ))}
       </div>
       <div>Scores pop here</div>
