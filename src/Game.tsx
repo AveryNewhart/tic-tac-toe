77c0899bdd109d53cd4e88242c2b458f3c4da857
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-15 13:58:13 -0400

    game box styling complete

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import Square from './Sqaure'
+import Square from './Square'
 
 const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""]
 
@@ -13,8 +13,7 @@ export default function Game() {
       <h1 className="text-center text-5xl mb-4 font-display text-white">Tic Tac Toe</h1>
       <div className='grid grid-cols-3 gap-3 mx-auto w-96'>
         {gameState.map((player, index) => (
-        <Square key={index}>
-          {player}</Square>
+        <Square key={index} {...{ player }}/>
       ))}
       </div>
       <div>Scores pop here</div>
